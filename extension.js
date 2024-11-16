ca7145ebb0b773d62c298dfee94f79271db9943e
Ahmad Jaenal <ahmadjaenal629@gmail.com>
2024-11-16 19:41:56 +0700

    feat : show list task

--- extension.js
+++ extension.js
@@ -10,25 +10,24 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Test');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('auto-unit-test.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Auto Unit Test!');
-	});
-
-	context.subscriptions.push(disposable);
+async function showMyTask() {
+	const dataList = [
+		'Tugas membuat fitur login user',
+		'Tugas membuat fitur registrasi user',
+		'Tugas membuat fitur update profil user',
+		'Tugas membuat fitur upload gambar',
+		'Tugas membuat fitur reset password',
+		'Tugas membuat fitur delete komentar',
+	];
+	const selectedItem = await vscode.window.showQuickPick(dataList, {
+		placeHolder: 'Select an item'
+	})
+	if (selectedItem) {
+		vscode.window.showInformationMessage(`You selected: ${selectedItem}`);
+	} else {
+		vscode.window.showInformationMessage('No item selected');
+	}
 }
-
-// This method is called when your extension is deactivated
-function deactivate() {}
 
 module.exports = {
 	activate,
