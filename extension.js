bb691d70462fd5922fca65f8d3824baa813213b3
Ahmad Jaenal <ahmadjaenal629@gmail.com>
2024-11-17 08:48:27 +0700

    feat: add view page

--- extension.js
+++ extension.js
@@ -49,7 +49,32 @@ async function selectCode() {
 		vscode.window.showErrorMessage('No text selected');
 		return;
 	}
-	vscode.window.showInformationMessage(`Selected text:${selectedText}`);
+function getWebviewContent(code) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Webview</title>
+        <style>
+            body { font-family: Arial, sans-serif; }
+            h1 { color: #007ACC; }
+        </style>
+    </head>
+    <body>
+        <h1>Laporan Tugas</h1>
+        <pre><code>${code}</code></pre>
+    </body>
+    </html>`;
+}
+
+function escapeHtml(unsafe) {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
 }
 
 function deactivate() { }
