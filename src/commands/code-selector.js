93e41e49418f0b910cb5fb1884b9d171f15c9421
AhmadJaenal <ahmadjaenal629@gmail.com>
2025-03-16 09:50:49 +0700

    refactor: Convert function-based module to class structur for improved maintainability

---
+++ src/commands/code-selector.js
@@ -0,0 +1,34 @@
+const vscode = require('vscode');
+const ControllerTestModule = require('./module-test/laravel/generate-test-case/controller-test-case');
+const RouteChecker = require('./module-test/laravel/route/route-checker');
+const MiddlewareChecker = require('./module-test/laravel/route/middleware-checker');
+
+class CodeSelector {
+    constructor() {
+        this.controllerTest = new ControllerTestModule();
+        this.routeChecker = new RouteChecker();
+        this.middlewareChecker = new MiddlewareChecker();
+    }
+
+    async selectCode() {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            vscode.window.showErrorMessage('No active editor found');
+            return;
+        }
+
+        const selection = editor.selection;
+        const selectedText = editor.document.getText(selection);
+
+        try {
+            const route = await this.routeChecker.checkRoute(selectedText);
+            const routeString = JSON.stringify(route);
+            const middleware = await this.middlewareChecker.executeCheckMiddleware(routeString);
+            this.controllerTest.generateControllerTest(selectedText, middleware, routeString);
+        } catch (error) {
+            console.error('Error in processing:', error);
+        }
+    }
+}
+
+module.exports = CodeSelector;
