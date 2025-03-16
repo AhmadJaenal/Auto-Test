e827b1e41154432b6ec8aef8dbd64dc86926f5e0
AhmadJaenal <ahmadjaenal629@gmail.com>
2025-03-16 10:02:08 +0700

    refactor: Convert function-base module to class structor to improve maintainability fix: Adjust filename to correctly reflect class name

---
+++ src/commands/task-manager.js
@@ -0,0 +1,47 @@
+const vscode = require('vscode');
+const CodeSelector = require('./code-selector');
+
+class TaskManager {
+    constructor() {
+        this.codeSelector = new CodeSelector();
+    }
+
+    async showTaskByProject(id) {
+        const apiKey = vscode.workspace.getConfiguration('auto-unit-test').get('apiKey');
+        const apiUrl = `http://127.0.0.1:8000/api/task/project/${id}`;
+
+        vscode.window.showInformationMessage(apiUrl);
+
+        try {
+            const response = await fetch(apiUrl, {
+                headers: {
+                    'API-Key': apiKey,
+                    'Content-Type': 'application/json'
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`Response Status: ${response.status}`);
+            }
+
+            const result = await response.json();
+            const data = result.data;
+
+            const projectList = data.map(item => item.title);
+            const selectedItem = await vscode.window.showQuickPick(projectList, {
+                placeHolder: 'Pilih tugas yang akan dilaporkan'
+            });
+
+            if (selectedItem) {
+                await this.codeSelector.selectCode();
+            } else {
+                vscode.window.showInformationMessage('Tidak ada yang dipilih');
+            }
+        } catch (error) {
+            vscode.window.showErrorMessage(error.message);
+            console.error(error.message);
+        }
+    }
+}
+
+module.exports = TaskManager;
