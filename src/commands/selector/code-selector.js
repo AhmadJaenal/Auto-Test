b3ed5bba7109ac85fc9326e16fa753a5e73d9345
AhmadJaenal <ahmadjaenal629@gmail.com>
2025-03-25 10:45:06 +0700

    feat: add function to detect code for model, controller, and migration

--- src/commands/selector/code-selector.js
+++ src/commands/selector/code-selector.js
@@ -1,13 +1,33 @@
 const vscode = require('vscode');
-const ControllerTestModule = require('../module-test/laravel/generate-test-case/controller-test-case');
+const GenerateTestModule = require('../module-test/laravel/generate-test-case/generate-test-case');
+
 const RouteChecker = require('../module-test/laravel/route/route-checker');
 const MiddlewareChecker = require('../module-test/laravel/route/middleware-checker');
 
+const ReadArtibutFactoryFile = require('../selector/core/read-atribut-factory-file');
+
 class CodeSelector {
+
+    static isFunction(code) {
+        const functionPattern = /public\s+function\s+\w+\s*\(/;
+        return functionPattern.test(code);
+    }
+
+    static isModel(code) {
+        const modelPattern = /class\s+\w+\s+extends\s+Model/;
+        return modelPattern.test(code);
+    }
+
+    static isMigration(code) {
+        const migrationPattern = /Schema::create\s*\(\s*'[^']+'\s*,\s*function\s*\(\s*Blueprint\s+\$table\s*\)\s*\{/;
+        return migrationPattern.test(code);
+    }
     async selectCode() {
-        const controllerTestModule = new ControllerTestModule();
+        const createUnitTest = new GenerateTestModule();
+
         const routeChecker = new RouteChecker();
         const middlewareChecker = new MiddlewareChecker();
+        const readArtibutFactoryFile = new ReadArtibutFactoryFile();
 
         const editor = vscode.window.activeTextEditor;
         if (!editor) {
@@ -22,14 +42,35 @@ class CodeSelector {
         }
 
         const selectedText = editor.document.getText(selection);
-      
 
-        try {
+        const isFunction = CodeSelector.isFunction(selectedText);
+        const isModel = CodeSelector.isModel(selectedText);
+        const isMigration = CodeSelector.isMigration(selectedText);
+
+        if (!isFunction && !isModel && !isMigration) {
+            vscode.window.showErrorMessage('Kode yang dipilih bukan merupakan fungsi dalam controller, model atau migration.');
+            return;
+        }
+
+        if (isFunction) {
             const route = await routeChecker.checkRoute(selectedText);
             const middleware = await middlewareChecker.executeCheckMiddleware(route);
-            controllerTestModule.generateControllerTest(selectedText, middleware, route);
-        } catch (error) {
-            console.error('Error in processing:', error);
+            createUnitTest.generateUnitTest({ code: selectedText, middleware: middleware, route: route });
+        }
+
+        if (isModel) {
+            readArtibutFactoryFile.getArtributFactory(selectedText)
+                .then(attributes => {
+                    vscode.window.showInformationMessage(`Hasil dari atribut factory ${attributes}`);
+                    createUnitTest.generateUnitTest({ code: selectedText });
+                })
+                .catch(error => {
+                    console.error('Error retrieving attributes:', error.message);
+                });
+        }
+
+        if (isMigration) {
+            createUnitTest.generateUnitTest(selectedText);
         }
     }
 }
