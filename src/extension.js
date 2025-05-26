8d1e1acb8ca88600d1f43be8f11da9f83058e1e4
AhmadJaenal <ahmadjaenal629@gmail.com>
2025-05-26 16:21:14 +0700

    refactor: remove unused code

--- src/extension.js
+++ src/extension.js
@@ -1,14 +1,8 @@
 const vscode = require('vscode');
 
 const ApiKeyHandler = require('./commands/module-test/api/api-key-handler');
-const ProjectService = require('./commands/services/project-service');
-
 const CodeSelector = require('./commands/selector/code-selector');
-const ModelFileReader = require('./commands/module-test/laravel/generate-factory/core/model-file-reader');
 const UnitTestManager = require('./commands/module-test/auto-test/unit-test-manager');
-
-const TestOpenAI = require('./commands/test');
-
 /**
  * @param {vscode.ExtensionContext} context
  */
@@ -19,11 +13,8 @@ class Main {
         this.context = context;
 
         this.apiKeyHandler = new ApiKeyHandler();
-        this.projectService = new ProjectService();
         this.codeSelector = new CodeSelector();
-        this.modelFileReader = new ModelFileReader();
         this.unitTestManager = new UnitTestManager();
-        this.testOpenAi = new TestOpenAI();
     }
 
     registerCommands() {
@@ -33,20 +24,12 @@ class Main {
             this.apiKeyHandler.updateApiKey();
         });
 
-        this.register('auto-unit-test.showProject', async () => {
-            await this.projectService.showProject();
-        });
-
         this.register('auto-unit-test.testController', async () => {
             this.codeSelector.selectCode({ isApiController: false });
         });
 
         this.register('auto-unit-test.testApiController', async () => {
             this.codeSelector.selectCode({ isApiController: true });
-        });
-
-        this.register('auto-unit-test.generateFactoryFile', async () => {
-            await this.modelFileReader.getModelFileNames();
         });
 
         this.register('auto-unit-test.runTestLaravel', () => {
