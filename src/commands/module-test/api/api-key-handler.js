d500fb63413c1e28fa84bdf9b2b954c2a5ee006b
AhmadJaenal <ahmadjaenal629@gmail.com>
2025-06-04 20:30:17 +0700

    feat: check key in database

--- src/commands/module-test/api/api-key-handler.js
+++ src/commands/module-test/api/api-key-handler.js
@@ -1,62 +1,60 @@
 const vscode = require('vscode');
 class ApiKeyHandler {
-    async updateApiKey() {
-        const config = vscode.workspace.getConfiguration('auto-unit-test');
-        let apiKey = config.get('apiKey');
-
-        const input = await vscode.window.showInputBox({
+    async inputKey() {
+        const key = await vscode.window.showInputBox({
             placeHolder: 'Masukkan API KEY',
             prompt: 'API KEY wajib diisi'
         });
 
-        if (input) {
-            const response = await fetch('http://localhost:8000/api/check-key', {
-                method: 'GET',
-                headers: {
-                    'X-API-Key': input,
-                    'Content-Type': 'application/json'
-                }
-            });
-
-            const data = await response.json();
-
-            if (response.status == 401) {
-                vscode.window.showInformationMessage('Key tidak terdaftar pada sistem');
-            }
-
-            if (response.status == 200) {
-                await config.update('apiKey', input, vscode.ConfigurationTarget.Global);
-                vscode.window.showInformationMessage('API KEY berhasil diperbarui!');
-            }
-
+        if (key) {
+            this.checkInputKey(key);
         } else {
             vscode.window.showErrorMessage('API KEY wajib ada');
         }
     }
+    
+    async checkInputKey(key) {
 
-    async askForApiKey() {
-        const config = vscode.workspace.getConfiguration('auto-unit-test');
-        let apiKey = config.get('apiKey');
-
-        if (!apiKey) {
-            const input = await vscode.window.showInputBox({
-                placeHolder: 'Masukkan API KEY',
-                prompt: 'API KEY wajib diisi'
+        try {
+            const response = await fetch('http://localhost:8000/api/check-key', {
+                method: 'GET',
+                headers: {
+                    'X-API-Key': key,
+                    'Content-Type': 'application/json'
+                }
             });
 
-            if (input) {
-                try {
-                    await config.update('apiKey', input, vscode.ConfigurationTarget.Global);
-                    vscode.window.showInformationMessage('API KEY berhasil disimpan!');
-                } catch (error) {
-                    vscode.window.showErrorMessage('Gagal menyimpan API KEY: ' + error.message);
-                }
-            } else {
-                vscode.window.showErrorMessage('API KEY wajib ada');
+            if (response.status == 200) {
+                await this.updateKey(key);
             }
-        } else {
-            vscode.window.showInformationMessage('API KEY saat ini: ' + apiKey);
+
+            if (response.status == 401) {
+                vscode.window.showInformationMessage('Key tidak terdaftar pada sistem');
+            }
+            if (response.status == 500) {
+                vscode.window.showInformationMessage('Terjadi kesalahan');
+            }
+        } catch (error) {
+            vscode.window.showErrorMessage('Terjadi kesalahan, silakan coba lagi nanti');
         }
+    }
+    
+    async updateKey(key) {
+        const config = vscode.workspace.getConfiguration('auto-unit-test');
+        await config.update('apiKey', key, vscode.ConfigurationTarget.Global);
+        vscode.window.showInformationMessage('API KEY berhasil disimpan!');
+    }
+
+    async checkKey() {
+        const config = vscode.workspace.getConfiguration('auto-unit-test');
+        let apiKey = config.get('apiKey');
+        return apiKey !== ' ';
+    }
+
+    async deleteKey() {
+        const config = vscode.workspace.getConfiguration('auto-unit-test');
+        await config.update('apiKey', ' ', vscode.ConfigurationTarget.Global);
+        vscode.window.showInformationMessage('API KEY berhasil dihapus!');
     }
 }
 
